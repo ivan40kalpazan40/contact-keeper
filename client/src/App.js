2cae26f99e958d18b7ea718dd976d26144bfd83f
Ivan Skordev <maymunskoa@yahoo.com>
2022-01-13 00:56:59 +0200

    install REACT and initial setup and cleanup

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,13 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p className='my-1'>This is the about page</p>
+      <p className='bg-danger p'>
+        <strong>Version:</strong>1.0.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
