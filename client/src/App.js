9a8518944fb6f5a2ceed8bec96e44de42a6a77e2
Ivan Skordev <maymunskoa@yahoo.com>
2022-01-15 20:55:27 +0200

    login and register components display

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
 
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
 function App() {
   return (
-    <ContactState>
-      <BrowserRouter>
-        <Navbar />
-        <div className='container'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <BrowserRouter>
+          <Navbar />
+          <div className='container'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </ContactState>
+    </AuthState>
   );
 }
 
