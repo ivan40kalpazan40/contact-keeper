f84a6275818e19474ab68e37d3b4c3d554fe9ece
Ivan Skordev <maymunskoa@yahoo.com>
2022-01-16 23:41:55 +0200

    guard privacy of home component

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -4,24 +4,31 @@ import ContactForm from '../contacts/ContactForm';
 import Contacts from '../contacts/Contacts';
 
 import AuthContext from '../../context/auth/authContext';
+import { Navigate } from 'react-router-dom';
 
 const Home = () => {
   const authContext = useContext(AuthContext);
-  const { loadUser } = authContext;
+  const { loadUser, isAuthenticated } = authContext;
   useEffect(() => {
     loadUser();
     //eslint-disable-next-line
   }, []);
   return (
-    <div className='grid-2'>
-      <div>
-        <ContactForm />
-      </div>
-      <div>
-        <ContactFilter />
-        <Contacts />
-      </div>
-    </div>
+    <>
+      {isAuthenticated ? (
+        <div className='grid-2'>
+          <div>
+            <ContactForm />
+          </div>
+          <div>
+            <ContactFilter />
+            <Contacts />
+          </div>
+        </div>
+      ) : (
+        <Navigate to='/login' />
+      )}
+    </>
   );
 };
 
