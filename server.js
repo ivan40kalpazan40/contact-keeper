e861fb980efca3770a123e66ba4685b9526d0ccb
Ivan Skordev <maymunskoa@yahoo.com>
2022-01-17 13:35:03 +0200

    heroku deploy setup

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const routes = require('./routes');
 const connectDB = require('./config/db');
@@ -11,4 +12,14 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 app.use(routes);
+
+// Serve static actions in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}....`));
